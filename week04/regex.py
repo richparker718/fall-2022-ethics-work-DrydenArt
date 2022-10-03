48c74d759e54378baf482949de1cffe47df29fc4
DrydenArt <45575196+DrydenArt@users.noreply.github.com>
2022-10-03 16:45:29 +0000

    edited regex.py

--- week04/regex.py
+++ week04/regex.py
@@ -1,18 +1,24 @@
 import re
 
 
-def find_date(line):
-    pattern = r"\d{1,2}/\d{1,2}/\d{2,4}"
-    result = re.findall(pattern,line)
-
-    pattern=r'(October|Oct|November|Nov)( [0-9]{1,2}, [0-9]{4})'
+def find_name(line):
+    #pattern = r"\d{1,2}/\d{1,2}/\d{2,4}"
+    pattern = r'\w[A-Za-z]+\s\w[A-Za-z]+'
+    result = re.findall(pattern, line)
+    
+    pattern = r'(?:Mr|Mrs|Ms|M|Miss|Madame|Mme|Dr)\.?\s+\w[A-Za-z]+\s+\w[A-Za-z]+'
     result = result + re.findall(pattern,line)
+
+  
+
+    #pattern=r'(October|Oct|November|Nov)( [0-9]{1,2}, [0-9]{4})'
+    #result = result + re.findall(pattern,line)
     return result
 
 
 f = open("names.txt")
 for line in f.readlines():
     #print(line)
-    result = find_date(line)
+    result = find_name(line)
     if (len(result)>0):
         print(result)
